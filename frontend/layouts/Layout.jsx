46393176a3a0df9cd27f51a8f4ea6221439474e3
Max Zlotskiy <max.zlotskiy@gmail.com>
2023-12-19 11:36:00 -0500

    added missing null check for sidebar ondestroy

--- frontend/layouts/Layout.jsx
+++ frontend/layouts/Layout.jsx
@@ -18,6 +18,7 @@ module.exports = {
 					</li>
 					<li><m.route.Link href="#!">Transactions</m.route.Link></li>
 					<li><m.route.Link href="#!">Transfers   </m.route.Link></li>
+					<li><m.route.Link href="/categories">Categories</m.route.Link></li>
 					<li><m.route.Link href="#!">Overview    </m.route.Link></li>
 				</ul>
 				<div class="container"><a href="#" data-target="sidenav" class="top-nav sidenav-trigger full hide-on-large-only"><i class="material-icons">menu</i></a></div>
@@ -30,6 +31,8 @@ module.exports = {
 		vnode.state.materialSidebarInstance = M.Sidenav.init(document.getElementById("sidenav"), {});
 	},
 	onremove: function(vnode) {
-		vnode.state.materialSidebarInstance.destroy();
+		if(vnode.state.materialSidebarInstance != null) {
+			vnode.state.materialSidebarInstance.destroy();
+		}
 	}
 }
