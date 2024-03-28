28f69076539c77d6ca3a870ffbc1d92451724b93
Max Zlotskiy <max.zlotskiy@gmail.com>
2024-03-28 01:37:49 -0400

    refactored methods for money formatting

--- frontend/models/index.js
+++ frontend/models/index.js
@@ -8,14 +8,40 @@ module.exports = {
 		return Category.getById(transaction["category"]);
 	},
 
-	formatTransactionAmount: function(transaction) {
-		const currency = Currency.getById(Wallet.getById(transaction["wallet"])["currency"]);
-		console.log("format transaction ", transaction);
-		console.log("format currency ", currency);
-		const amount = transaction["money"].toString();
+	/**
+	 * Gets the currency used by a transaction. Precondition: the transaction's
+	 * wallet and currency must be loaded.
+	 * @param transaction  a transaction with a wallet id
+	 * @return  a currency object
+	 */
+	getCurrencyOfTransaction: function(transaction) {
+		return Currency.getById(Wallet.getById(transaction["wallet"])["currency"]);
+	},
+
+	/**
+	 * Formats a money amount as human-readable. Precondition: the currency must
+	 * be loaded.
+	 * @param money  a positive integer, not premultiplied by the currency's 
+	 * cents/dollars system
+	 * @param currencyId  the string id of the currency
+	 * @return  string
+	 */
+	formatMoneyAmount: function(money, currencyId) {
+		const currency = Currency.getById(currencyId);
+		let amount = (typeof money == "number")? money.toString() : money;
+		amount = amount.padStart(currency["decimals"] + 1, "0"); // pad with leading zeroes
 		return currency["symbol"] + 
 			amount.substring(0, amount.length - currency["decimals"]) + 
 			"." +
 			amount.substring(amount.length - currency["decimals"]);
 	},
+
+	/**
+	 * Formats a money amount depending on the currency. For example, an input 
+	 * of {money: 100,  currency: USD} becomes "$1.00"
+	 */
+	formatTransactionAmount: function(transaction) {
+		return this.formatMoneyAmount(transaction["money"], Wallet.getById(transaction["wallet"])["currency"]);
+	},
+
 };
