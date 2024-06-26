7cf64b2692a3613dc2d1b42270e2aa8907bb4210
Max Zlotskiy <max.zlotskiy@gmail.com>
2024-06-26 17:55:29 -0400

    fetch less data by ommitting collection name

--- frontend/models/Report.js
+++ frontend/models/Report.js
@@ -3,7 +3,8 @@ const pb = require("../api")
 const Report = {
 	getNetWorth: function(wallet) {
 		return pb.collection("net_worth").getFullList({
-			wallet: wallet
+			wallet: wallet,
+			fields: "id,date,users,wallets,cashflow,balance"
 		});
 	}
 };
