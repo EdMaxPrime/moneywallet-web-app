d000803544e5258b656a804409f88cb6d9303a55
Max Zlotskiy <max.zlotskiy@gmail.com>
2024-06-26 17:56:00 -0400

    chart may be updated

--- frontend/components/HighchartsContainer.jsx
+++ frontend/components/HighchartsContainer.jsx
@@ -8,6 +8,10 @@ const m = require("mithril");
  * 
  * chartCreatedCallback = [optional] function called when the chart is created,
  * first argument to the function is the Highcharts chart instance
+ * 
+ * allowChartUpdate = [default false]. When true, this will update the chart
+ * whenever this component's onUpdate() lifecycle method runs after redraw.
+ * Incudes chart options and data.
  */
 
 module.exports = function() {
@@ -17,11 +21,19 @@ module.exports = function() {
 		oncreate: function(vnode) {
 			chart = window.Highcharts.chart(vnode.dom, vnode.attrs.chartOptions, vnode.attrs.chartCreatedCallback);
 		},
-		// onupdate: function(vnode) {
-			// call chart.update(vnode.attrs.chartOptions) with new attribute
-			// however, if chartUpdate=false then ignore attribute change
-			// when updating the argument, use a deepmerge
-		// },
+		/**
+		 * Todo: make this component more reactive by performing an update only
+		 * if the options object has changed. This can be checked with a
+		 * reference to the previous options pointer. This component should be
+		 * updated by Object.assign() to the options.
+		 */
+		onupdate: function(vnode) {
+			if(vnode.attrs.allowChartUpdate === true && chart != null) {
+				chart.update(vnode.attrs.chartOptions, 
+					true, // redraw chart (not Mithril) after update
+					true); // oneToOne: remove/update series instead of appending
+			}
+		},
 		onremove: function(vnode) {
 			if(chart != null) {
 				chart.destroy();
