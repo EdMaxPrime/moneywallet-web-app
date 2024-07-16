28c23320250225d38b8c82df42238b84bb5e9205
Max Zlotskiy <max.zlotskiy@gmail.com>
2024-07-16 16:40:18 -0400

    added user settings to frontend

---
+++ frontend/models/Settings.js
@@ -0,0 +1,7 @@
+const Settings = {
+	first_week_day : 1, //1=Monday, 7=Sunday
+	first_month_date : 1, //1-31
+	financial_year_first_month : 1, //1=January, 12=December
+};
+
+module.exports = Settings;
