e61511521457642002cf9e3dc38f6e4103bec621
Max Zlotskiy <max.zlotskiy@gmail.com>
2024-07-31 15:01:28 -0400

    using paged table for transactions page

--- frontend/models/Transaction.js
+++ frontend/models/Transaction.js
@@ -13,6 +13,24 @@ var Transaction = {
 	},
 
 	/**
+	 * Retrieves some transaction records. The response is not cached, so the
+	 * caller should have their own caching strategy.
+	 * @param page  the results are paginated, and this is the 1-based page number
+	 * @param perPage  the number of items per page. Result may have fewer
+	 * @param options  and object with filter, sort, expand, and fields. Consult
+	 * the PocketBase API documentation for details
+	 * @return a Promise that resolves to a JSON object. On success, it will
+	 * have these fields:
+	 *   totalItems: the total number of items, even beyond this page
+	 *   totalPages: the total number of pages
+	 *   page: the current page number
+	 *   items: a list of Transaction records
+	 */
+	loadSome: function(page, perPage, options) {
+		return pb.collection("transactions").getList(page, perPage, options)
+	},
+
+	/**
 	 * Retrieves transactions that belong to a set of categories
 	 * @param categories a list of strings, each is a category id
 	 */
--- frontend/pages/transactions.jsx
+++ frontend/pages/transactions.jsx
@@ -1,19 +1,29 @@
 const m = require("mithril")
 const TransactionList = require("../components/TransactionList")
+const TransactionListPageable = require("../components/TransactionListPageable")
 
 const Transaction = require("../models/Transaction")
 
 
+// module.exports = {
+// 	oninit: function() {
+// 		Promise.all([
+// 			Transaction.loadList()
+// 			]).then(m.redraw)
+// 		.catch(reason => console.log("Promise failed ", reason));
+// 	},
+// 	view: function() {
+// 		return (
+// 				<TransactionList transactions={Transaction.list} />
+// 			);
+// 	}
+// }
+
 module.exports = {
-	oninit: function() {
-		Promise.all([
-			Transaction.loadList()
-			]).then(m.redraw)
-		.catch(reason => console.log("Promise failed ", reason));
-	},
 	view: function() {
-		return (
-				<TransactionList transactions={Transaction.list} />
-			);
+		return (<TransactionListPageable 
+			arg={{sort: '-date'}} 
+			fetch={Transaction.loadSome}
+			showTotal={true} />);
 	}
 }
