702bd62626fb14dc4899361cd3478473b02cf8da
Max Zlotskiy <max.zlotskiy@gmail.com>
2025-05-22 16:12:40 -0400

    added utility function and tests

---
+++ frontend/tests/util.test.js
@@ -0,0 +1,86 @@
+const o = require("ospec");
+
+const Util = require("../util/index");
+
+o.spec("Group By", function() {
+	const data = [
+		{index: 0, b: false},
+		{index: 1, b: true},
+		{index: 2, b: false}
+	];
+
+	o("group by boolean key", function() {
+		const result = Util.groupBy(data, ["b"], function(groupNames, items) {
+			return {groupNames: groupNames, items: items};
+		});
+
+		o(result.length).equals(2)("2 groups");
+		o(result[0].groupNames.length).equals(1); // 1 group
+		o(result[1].groupNames.length).equals(1); // 1 group
+		o(result[0].items.length + result[1].items.length).equals(3)("3 items total");
+		o(result[0].groupNames[0]).notEquals(result[1].groupNames[0])("group names are different");
+	});
+
+	o("group by boolean key and sum", function() {
+		const result = Util.groupBy(data, ["b"], function(groupNames, items) {
+			return {
+				odd: groupNames[0],
+				sum: items.map(object => object.index).reduce((sum, num) => sum+num, 0)
+			};
+		});
+
+		o(result.length).equals(2)("2 groups");
+		o(result[0].odd).notEquals(result[1].odd)("not both odd");
+		o(result[0].sum + result[1].sum).equals(3)("sum 3");
+
+		result.sort(function(a, b) {
+			if(a.odd) return -1;
+			return 1;
+		});
+
+		o(result[0].sum).equals(1)("odd first");
+		o(result[1].sum).equals(2)("even second");
+	});
+
+	const data2 = [
+		{direction: 0, category: "a", money: 1},
+		{direction: 0, category: "b", money: 2},
+		{direction: 0, category: "a", money: 10},
+		{direction: 0, category: "b", money: 20},
+		{direction: 1, category: "a", money: 100},
+		{direction: 1, category: "b", money: 200},
+		{direction: 1, category: "a", money: 1000},
+		{direction: 1, category: "b", money: 2000},
+	];
+
+	o("group by number and string", function() {
+		const result = Util.groupBy(
+			data2, 
+			["direction", "category"], 
+			function(groupNames, items) {
+				return {
+					direction: groupNames[0], 
+					category: groupNames[1],
+					money: items.reduce((sum, object) => sum+object.money, 0)
+				};
+			}
+		);
+
+		o(result.length).equals(4)("4 groups");
+		
+		result.sort(function(a, b) {
+			if(a.direction < b.direction) return -1;
+			else if(a.direction > b.direction) return 1;
+			else {
+				if(a.category < b.category) return -1;
+				return 1;
+			}
+		});
+
+		o(result[0]).deepEquals({direction: '0', category: "a", money: 11});
+		o(result[1]).deepEquals({direction: '0', category: "b", money: 22});
+		o(result[2]).deepEquals({direction: '1', category: "a", money: 1100});
+		o(result[3]).deepEquals({direction: '1', category: "b", money: 2200});
+	});
+
+})
---
+++ frontend/util/index.js
@@ -0,0 +1,71 @@
+
+/**
+ * Map and reduce a list of objects with the same structure.
+ * @param list  the items
+ * @param groupedKeys  a list of criteria to group objects. This list can't be
+ * empty. Each criteria can be a (string) object key, then identical values for
+ * the key will be grouped together. Or, it can be a function, which takes 1
+ * object as a parameter and must return a string group name.
+ * @param aggregate  a function that converts a group into a single object/value.
+ * It accepts two parameters: 
+ * (1) a list of group names, in the same order as groupedKeys
+ * (2) a list of items in that group.
+ * And returns one object/value representing the group.
+ * @return list of objects/values, one per group.
+ */
+const groupBy = function(list, groupedKeys, aggregate) {
+	let tree = {};
+	// transform all grouping keys to functions to make them easier to invoke
+	const groupingFunctions = groupedKeys.map(function(key) {
+		if(typeof key === "string") {
+			return function(object) {return object[key];}
+		}
+		return key;
+	});
+	// (1) loop through list
+	for(item of list) {
+		let pointer = tree;
+		// (2) run each grouping function on the item, build the tree of nested groups
+		for(let i = 0; i < groupingFunctions.length; i++) {
+			// get group name
+			let groupName = groupingFunctions[i](item);
+			// if this group hasn't been encountered in this tree, create the structure
+			if(!(groupName in pointer)) {
+				// at the leaf level, create an array to hold items
+				if(i == groupingFunctions.length-1) {
+					pointer[groupName] = [];
+				} else { // otherwise, create a subtree to hold more groups
+					pointer[groupName] = {};
+				}
+			}
+			// move pointer down the tree
+			pointer = pointer[groupName];
+		}
+		// (3) sort item into groups
+		pointer.push(item);
+	}
+
+	const recursiveDepthFirstTraversal = function(subtree, groupNames) {
+		// if we reached the leaves of the tree, the list of items, then run aggregate function
+		if(groupNames.length == groupedKeys.length) {
+			return [aggregate(groupNames, subtree)];
+		}
+		// otherwise, recursively call this function on each subtree, and collect the groups into a list
+		else {
+			let results = [];
+			for(let groupName in subtree) {
+				results = results.concat(recursiveDepthFirstTraversal(subtree[groupName], groupNames.concat([groupName])));
+			}
+			return results;
+		}
+	}
+
+	// (4) loop through groups
+		// (5) produce one object that has all the keys from the aggregate template
+	// return a list of objects, one per group
+	return recursiveDepthFirstTraversal(tree, []);
+}
+
+module.exports = {
+	groupBy: groupBy,
+};
