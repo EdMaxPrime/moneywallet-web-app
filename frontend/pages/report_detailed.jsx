33e5b0004d10970c3376706baebfe5100be29c7a
Max Zlotskiy <max.zlotskiy@gmail.com>
2025-05-22 17:26:05 -0400

    added tooltips and support for multiple currencies as multiple rows of charts

--- frontend/pages/report_detailed.jsx
+++ frontend/pages/report_detailed.jsx
@@ -4,10 +4,13 @@ const HighchartsContainer = require("../components/HighchartsContainer")
 
 // Data model imports
 const Category = require("../models/Category")
+const Currency = require("../models/Currency")
 const Report = require("../models/Report")
+const Util = require("../models/index")
 
 // Utility imports
 const dayjs = require("../dayjs-lib")
+const util = require("../util")
 
 
 /** Names for status constants for AJAX request */
@@ -16,109 +19,75 @@ READY = 3,
 ERROR = 4
 ERROR_PARAMETERS = 5;
 
+/**
+ * Creates the data structure for Highcharts pie chart.
+ * @param title  the bolded title above the chart
+ * @param data   list of points with two properties: name (string) and y (number)
+ * @param currencyId  this identifier instructs the tooltip formatter how to
+ *                    show currency-specific numbers
+ * @return  object
+ */
+const createPieChartOptions = function(title, data, currencyId) {
+	return {
+		chart: {
+			type: "pie",
+		},
+		credits: {
+			enabled: false,
+		},
+		plotOptions: {
+			series: {
+				allowPointSelect: true,
+				cursor: "pointer",
+				dataLabels: [{
+	                enabled: true,
+	                distance: 20
+	            }, {
+	                enabled: true,
+	                distance: -40,
+	                format: '{point.percentage:.1f}%',
+	                style: {
+	                    fontSize: '1.2em',
+	                    textOutline: 'none',
+	                    opacity: 0.7
+	                },
+	                filter: {
+	                    operator: '>',
+	                    property: 'percentage',
+	                    value: 10
+	                }
+	            }]
+			},
+		},
+		series: [
+			{
+				name: "Percentage",
+				colorByPoint: true,
+				data: data,
+			}
+		],
+		subtitle: {
+			text: "Only parent categories are shown, and they include the sum of child categories",
+		},
+		title: {
+			text: title,
+		},
+		tooltip: {
+			pointFormatter: function() {
+				return this.series.name + ": <b>" + Util.formatMoneyAmount(Math.abs(this.y), Currency.getById(currencyId)) + "</b><br/>";
+			},
+		},
+	};
+};
+
 module.exports = function(initialVnode) {
 	// any variables declared here are part of the component's state
 	// all code executed before the return statement happens once when it is created (not on recycle)
 	let status = WAITING; // status of network request
 	let message = "";
 
-	let expensePieChartOptions = {
-		chart: {
-			type: "pie",
-		},
-		credits: {
-			enabled: false,
-		},
-		plotOptions: {
-			series: {
-				allowPointSelect: true,
-				cursor: "pointer",
-				dataLabels: [{
-	                enabled: true,
-	                distance: 20
-	            }, {
-	                enabled: true,
-	                distance: -40,
-	                format: '{point.percentage:.1f}%',
-	                style: {
-	                    fontSize: '1.2em',
-	                    textOutline: 'none',
-	                    opacity: 0.7
-	                },
-	                filter: {
-	                    operator: '>',
-	                    property: 'percentage',
-	                    value: 10
-	                }
-	            }]
-			},
-		},
-		series: [
-			{
-				name: "Percentage",
-				colorByPoint: true,
-				data: [],
-			}
-		],
-		subtitle: {
-			text: "Only parent categories are shown, and they include the sum of child categories",
-		},
-		title: {
-			text: "Expenses by Category",
-		},
-		tooltip: {
-			valuePrefix: "$",
-		},
-	};
+	let categoryPieCharts = []; //holds rows of data structured as {currencyName, expensePieChartOptions, incomePieChartOptions}
 
-	let incomePieChartOptions = {
-		chart: {
-			type: "pie",
-		},
-		credits: {
-			enabled: false,
-		},
-		plotOptions: {
-			series: {
-				allowPointSelect: true,
-				cursor: "pointer",
-				dataLabels: [{
-	                enabled: true,
-	                distance: 20
-	            }, {
-	                enabled: true,
-	                distance: -40,
-	                format: '{point.percentage:.1f}%',
-	                style: {
-	                    fontSize: '1.2em',
-	                    textOutline: 'none',
-	                    opacity: 0.7
-	                },
-	                filter: {
-	                    operator: '>',
-	                    property: 'percentage',
-	                    value: 10
-	                }
-	            }]
-			},
-		},
-		series: [
-			{
-				name: "Percentage",
-				colorByPoint: true,
-				data: [],
-			}
-		],
-		subtitle: {
-			text: "Only parent categories are shown, and they include the sum of child categories",
-		},
-		title: {
-			text: "Income by Category",
-		},
-		tooltip: {
-			valuePrefix: "$",
-		},
-	};
 
 	// Fetch data from API
 	let startDate = dayjs(initialVnode.attrs.startDate, "YYYY-MM-DD");
@@ -127,34 +96,49 @@ module.exports = function(initialVnode) {
 	if (startDate.isValid() && endDate.isValid() && startDate.isBefore(endDate)) {
 		Report.getMoneyPerCategory(initialVnode.attrs.startDate, initialVnode.attrs.endDate)
 			.then(data => {
-				// ignore currencies for now
-				let expenseData = data.filter(
-						item => Category.getDirection(item.categoryId) == Category.DIRECTION_EXPENSE
-					).map(
-						item => {
-							return {
-								name: Category.getById(item.categoryId).name,
-								y: item.money,
-							}
-						}
-					).sort(function(a, b) {
-						return a.y - b.y;
-					});
-				expensePieChartOptions.series[0].data = expenseData;
+				// sort data into groups by category, and transform it into HighCharts pie chart format
+				categoryPieCharts = util.groupBy(
+					data, 
+					["currencyId"], 
+					function(groupNames, recordsInGroup) {
 
-				let incomeData = data.filter(
-						item => Category.getDirection(item.categoryId) == Category.DIRECTION_INCOME
-					).map(
-						item => {
-							return {
-								name: Category.getById(item.categoryId).name,
-								y: item.money,
-							}
-						}
-					).sort(function(a, b) {
-						return a.y - b.y;
+						// this will be the item in categoryPieCharts list, used to draw the UI in a loop
+						let categoryPieChartsRow = {
+							currencyName: Currency.getById(groupNames[0]).name,
+						};
+
+						// create data structure for expense category pie chart
+						let expenseData = recordsInGroup.filter(
+								item => Category.getDirection(item.categoryId) == Category.DIRECTION_EXPENSE
+							).map(
+								item => {
+									return {
+										name: Category.getById(item.categoryId).name,
+										y: item.money,
+									}
+								}
+							).sort(function(a, b) {
+								return a.y - b.y;
+							});
+						categoryPieChartsRow.expensePieChartOptions = createPieChartOptions("Expenses by Category", expenseData, groupNames[0]);
+
+						// create data structure for income category pie chart
+						let incomeData = recordsInGroup.filter(
+								item => Category.getDirection(item.categoryId) == Category.DIRECTION_INCOME
+							).map(
+								item => {
+									return {
+										name: Category.getById(item.categoryId).name,
+										y: item.money,
+									}
+								}
+							).sort(function(a, b) {
+								return a.y - b.y;
+							});
+						categoryPieChartsRow.incomePieChartOptions = createPieChartOptions("Income by Category", incomeData, groupNames[0]);
+
+						return categoryPieChartsRow;
 					});
-				incomePieChartOptions.series[0].data = incomeData;
 
 				status = READY;
 			})
@@ -172,20 +156,21 @@ module.exports = function(initialVnode) {
 	return {
 		view: function(vnode) {
 			if (status == READY) {
-				return (
-					<div class="row">
+				return categoryPieCharts.map(categoryPieChartsRow => (
+					<div class="row" key={categoryPieChartsRow.currencyName}>
+						<h3 class="col s12">{categoryPieChartsRow.currencyName}</h3>
 						<div class="col s12 m6">
-							<HighchartsContainer 
-								chartOptions={expensePieChartOptions} 
+							<HighchartsContainer
+								chartOptions={categoryPieChartsRow.expensePieChartOptions}
 								allowChartUpdate={false} />
 						</div>
 						<div class="col s12 m6">
-							<HighchartsContainer 
-								chartOptions={incomePieChartOptions} 
+							<HighchartsContainer
+								chartOptions={categoryPieChartsRow.incomePieChartOptions}
 								allowChartUpdate={false} />
 						</div>
 					</div>
-				);
+				));
 			}
 			else if (status == WAITING) {
 				return (
