b3479d6e6fca968f12699750db14b7a6306ae7a9
Max Zlotskiy <max.zlotskiy@gmail.com>
2025-10-20 16:23:35 -0400

    rewrite to use column chart and not load its own data

--- frontend/components/TransactionHistogram.jsx
+++ frontend/components/TransactionHistogram.jsx
@@ -1,130 +1,226 @@
 const m = require("mithril")
 // Mithril component imports
-const HighchartsContainer = require("../components/HighchartsContainer")
+const CategoryPicker = require("./CategoryPicker")
+const HighchartsContainer = require("./HighchartsContainer")
+const {Button, ModalPanel} = require("mithril-materialized")
 
 // Data model imports
 const Category = require("../models/Category")
+const Currency = require("../models/Currency")
 const Transaction = require("../models/Transaction")
-const Util = require("../models/index")
+const Model = require("../models/index")
 
-
-/** Names for status constants for AJAX request */
-const WAITING = 2, 
-READY = 3, 
-ERROR = 4;
+// data manipulation imports
+const Util = require("../util/index")
 
 
 
 /**
- * Displays a histogram, which groups transactions into buckets based on the
- * money amount.
+ * Calculates the ideal bin width for a histogram chart. There will be at least
+ * 10 bins, so the user can notice trends (too few bins are hard to read). The
+ * bins will also fall on natural boundaries, prefering multiples of 10 and 5.
  * 
- * Attributes:
- * @attribute startDate  the start of the date range to filter data. String
- * formatted as YYYY-MM-DD
- * @attribute startDate  the end of the date range to filter data. String
- * formatted as YYYY-MM-DD
+ * @param transactions  the list of Transaction objects to base calculations on
+ * @return  a positive integer, in money units, of the ideal bin width
  */
-module.exports = function(initialVnode) {
-	// any variables declared here are part of the component's state
-	// all code executed before the return statement happens once when it is created (not on recycle)
-	let status = WAITING; // status of network request
-	let message = "";
+const calculateBinWidth = function(transactions) {
+	let min = -1, max = 0;
+	for(let i = 0; i < transactions.length; i++) {
+		if(min == -1 || transactions[i].money < min)
+			min = transactions[i].money;
+		if(transactions[i].money > max)
+			max = transactions[i].money;
+	}
+	let range = max - min;
+	let binWidth = Math.pow(10, Math.floor(Math.log10(range))); // find the largest power of 10 that's smaller than range (ex: if range=600, then breakpoint=100)
+	let binsNumber = Math.ceil(range / binWidth);
+	while(binsNumber < 10) {
+		binWidth = binWidth % 2 == 0? (binWidth / 2) : Math.pow(10, Math.floor(Math.log10(binWidth)));
+		binsNumber = Math.ceil(range / binWidth);
+	}
 
-	let amounts = [];
+	return binWidth;
+}
 
-	Transaction.getByDates(initialVnode.attrs.startDate, initialVnode.attrs.endDate).then(function(data) {
-		amounts = data.filter(t => t.direction === false).map(t => t.money);
-		status = READY;
-	})
-	.catch(function(error) {
-		status = ERROR;
-	})
-	.finally(m.redraw);
+/**
+ * Converts a list of transactions into a list of histogram chart points. The
+ * transactions are grouped by their money value into buckets defined by
+ * binWidth. For example, if binWidth=10, then the buckets would be:
+ *   bucket 0: $0 to $10
+ *   bucket 1: $10 to $20
+ *   bucket 2: $20 to $30
+ * 
+ * @param transactions  list of Transaction objects
+ * @param binWidth  positive integer
+ * @return  a Highcharts series data. It is a list of points. Each point is a
+ * list of two numbers: the first is the minimum of the bucket's range, in money,
+ * and the second is the number of transactions that are in that bucket (y).
+ */
+const groupTransactions = function(transactions, binWidth) {
+	return Util.groupBy(
+		transactions, 
+		[
+			(transaction) => (binWidth * Math.floor(transaction.money / binWidth))|0
+		],
+		(binMin, transactionsInBin) => {
+			return [parseInt(binMin[0]), transactionsInBin.length]
+		});
+}
+
+/**
+ * Creates the series and xAxis arrays for the Highcharts chart. The parameter
+ * transactions should be filtered for relevant categories.
+ * 
+ * @param transactions  list of Transaction objects.
+ * @return  an object with two arrays: series and xAxis
+ */
+const createSeriesAndAxis = function(transactions) {
+	let allSeries = [];
+
+	let allXAxis = Util.groupBy(
+		transactions,
+		[
+			(transaction) => Model.getCurrencyOfTransaction(transaction).id,
+		],
+		(groups, itemsWithThisCurrency) => {
+			const binWidth = calculateBinWidth(itemsWithThisCurrency);
+			const currencyId = groups[0];
+
+			// take advantage of the fact that the data is already grouped by currency to create series
+			allSeries = allSeries.concat(Util.groupBy(
+				itemsWithThisCurrency,
+				["category"],
+				(category, itemsWithThisCategory) => {
+					return {
+						data: groupTransactions(itemsWithThisCategory, binWidth),
+						id: category[0] + currencyId,
+						name: Category.getById(category[0]).name + " (" + Currency.getById(currencyId).name + ")",
+						type: "column",
+						opacity: 0.75,
+						pointRange: binWidth,
+						tooltip: {
+							headerFormat: "{series.name}<br/>",
+							pointFormatter: function() {
+								return '<span style="color:' + this.color + '">•</span> ' + Model.formatMoneyAmount(parseInt(this.x), currencyId) + " to " + Model.formatMoneyAmount(parseInt(this.x) + binWidth, currencyId) + " : " + this.y + " transactions";
+							},
+						},
+						xAxis: currencyId,
+					};
+				}
+			));
+
+			// create xAxis object, to be included in the array
+			return {
+				id: currencyId,
+				min: 0,
+				labels: {
+					enabled: true,
+					formatter: function() {
+						return Model.formatMoneyAmount(parseInt(this.value), currencyId);
+					},
+				},
+				startOnTick: true,
+				tickInterval: binWidth,
+				title: { text: Currency.getById(currencyId).name },
+			};
+		}
+	);
 
 	return {
-		view: function(vnode) {
-			if (status == READY) {
-				return (<div>
-					<HighchartsContainer 
-						chartOptions={{
-							credits: {
-								enabled: false,
-							},
-							legend: {
-								enabled: false,
-							},
-							title: {
-								text: "Transaction Value Frequency",
-							},
-							xAxis: [{
-								title: { text: 'Money' },
-								alignTicks: false,
-								formatter: function() {
-									return Util.formatMoneyAmount(parseInt(this.value), "lv25l90oi4x8sz3")
-								},
-							}, {
-								visible: false,
-							}],
-
-							yAxis: [{
-								title: { text: 'Data' }
-							}, {
-								title: { text: 'Histogram' },
-								opposite: true
-							}],
-
-							plotOptions: {
-								histogram: {
-									accessibility: {
-										point: {
-											valueDescriptionFormat: '{index}. {point.x:.3f} to {point.x2:.3f}, {point.y}.'
-										}
-									}
-								}
-							},
-
-							tooltip: {
-								formatter: function(point) {
-									return Util.formatMoneyAmount(parseInt(this.point.x), "lv25l90oi4x8sz3") + " to " + Util.formatMoneyAmount(parseInt(this.point.x2), "lv25l90oi4x8sz3") + " : " + this.y + " transactions";
-								},
-							},
-
-							series: [{
-								name: 'Histogram',
-								type: 'histogram',
-								xAxis: 1,
-								yAxis: 1,
-								baseSeries: 's1',
-								zIndex: -1,
-								binWidth: 10000
-							}, {
-								name: 'Data',
-								type: 'scatter',
-								data: amounts,
-								id: 's1',
-								marker: {
-									radius: 1.5
-								},
-								visible: false,
-							}]
-							}}
-						allowChartUpdate={false} />
-					</div>);
-			}
-			else if (status == WAITING) {
-				return (
-					<p>Generating chart...</p>
-				);
-			}
-			else if (status == ERROR) {
-				return (
-					<div class="red lighten-3">
-						<h3>Failed to generate report</h3>
-						<p>Please check your internet connection and reload the page. Otherwise, our servers may be having difficulty.</p>
-					</div>
-				);
-			}
-		}
+		series: allSeries,
+		xAxis: allXAxis,
 	};
 }
+
+/**
+ * This component is implemented as POJO. The state is used to cache computationally
+ * expensive data for Highcharts. The component renders a div with a chart and a
+ * button. The chart is a histogram of transactions grouped by their money value.
+ * The button opens a modal with a category picker, which is used to add a series
+ * to the chart. Each category is a series. Multiple can be shown. Works with
+ * multiple currencies.
+ * 
+ * Attributes:
+ * @attribute data  a list of Transaction objects
+ */
+module.exports = {
+	oninit: function(vnode) {
+		vnode.state.series = [];
+		vnode.state.xAxis = [];
+		vnode.state.transactions = [];
+		vnode.state.categories = [];
+		vnode.state.modalOpen = false;
+	},
+	view: function(vnode) {
+		if(vnode.state.transactions != vnode.attrs.data) {
+			vnode.state.transactions = vnode.attrs.data;
+			const calculated = createSeriesAndAxis(vnode.state.transactions.filter(transaction => vnode.state.categories.indexOf(transaction.category) != -1));
+			vnode.state.series = calculated.series;
+			vnode.state.xAxis = calculated.xAxis;
+		}
+
+		return m("div.card-panel", [
+			m(HighchartsContainer, {
+				chartOptions: {
+					credits: {
+						enabled: false,
+					},
+					plotOptions: {
+						column: {
+							pointPadding: 0, // make column chart look like a histogram
+					        borderWidth: 0, // make column chart look like a histogram
+					        groupPadding: 0, // make column chart look like a histogram
+					        shadow: false, // make column chart look like a histogram
+					        pointPlacement: "between", // histogram columns should be between tick marks for readability
+						},
+					},
+					series: vnode.state.series,
+					title: {
+						text: "Transaction Distribution",
+					},
+					subtitle: {
+						text: "Histogram of the value of each transaction. Use the button below to toggle which categories are shown.",
+						align: "center",
+					},
+					xAxis: vnode.state.xAxis,
+					yAxis: {
+						title: {text: "Frequency"}
+					}
+				},
+				allowChartUpdate: true,
+			}),
+			m(Button, {
+				label: "Add category",
+				onclick: () => {vnode.state.modalOpen = true;},
+			}),
+
+			m(ModalPanel, {
+				title: "Select categories to show in chart",
+				description: m("div", [
+					m(CategoryPicker, {
+						multiple: true,
+						showMetaCategories: false,
+						selectedIds: vnode.state.categories,
+						onselection: (selectedIds) => {
+							if(selectedIds.length > 0) vnode.state.categories = selectedIds;
+						}
+					}),
+				]),
+				isOpen: vnode.state.modalOpen,
+				onToggle: open => {
+					vnode.state.modalOpen = open; 
+				},
+				buttons: [
+					{
+						label: "Add",
+						onclick: () => { // updated series and axes when new categories are added to the chart
+							const calculated = createSeriesAndAxis(vnode.state.transactions.filter(transaction => vnode.state.categories.indexOf(transaction.category) != -1));
+							vnode.state.series = calculated.series;
+							vnode.state.xAxis = calculated.xAxis;
+						},
+					}
+				],
+			}),
+		]);
+	},
+};
