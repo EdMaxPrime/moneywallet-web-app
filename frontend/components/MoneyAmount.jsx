db08b8b3f7738afba70853917f9670451ee75077
Max Zlotskiy <max.zlotskiy@gmail.com>
2025-11-30 19:51:14 -0500

    catch exception

--- frontend/components/MoneyAmount.jsx
+++ frontend/components/MoneyAmount.jsx
@@ -5,10 +5,15 @@ const Util = require("../models/index.js")
 
 /**
  * Attributes: "direction" and ("t" or ("money" and "currencyId"))
- * direction = 0 for income styling, 1 for expense styling
- * money = a positive integer (not premultiplied by the currency's decimal)
- * currencyId = a string id of the currency this amount is in
- * t = a Transaction model object
+ * @attribute direction = 0 for income styling, 1 for expense styling
+ * 
+ * @attribute money = a positive integer (not premultiplied by the currency's decimal)
+ * @attribute currencyId = a string id of the currency this amount is in
+ * 
+ * @attribute t = a Transaction model object
+ * 
+ * @attribute color (optional, default true) true to add color styling
+ * based on the direction, false to leave black
  * 
  * Example: 
  * 		<MoneyAmount direction={1} t={transaction} />
@@ -16,11 +21,19 @@ const Util = require("../models/index.js")
  */
 module.exports = {
 	view: function(vnode) {
+		let text;
+		try {
+			text = ("t" in vnode.attrs)? Util.formatTransactionAmount(vnode.attrs.t) : Util.formatMoneyAmount(vnode.attrs.money, vnode.attrs.currencyId);
+		} catch(e) {
+			text = "?"; //default
+		}
+		const color = !("color" in vnode.attrs && vnode.attrs.color === false);
+
 		return m("span", 
 			{
-				className: vnode.attrs.direction == 0? "red-text text-darken-1" : "cyan-text text-darken-4",
+				className: color? (vnode.attrs.direction == 0? "red-text text-darken-1" : "cyan-text text-darken-4") : null,
 			},
-			("t" in vnode.attrs)? Util.formatTransactionAmount(vnode.attrs.t) : Util.formatMoneyAmount(vnode.attrs.money, vnode.attrs.currencyId)
+			text
 		);
 	}
 };
