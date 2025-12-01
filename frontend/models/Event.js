2bb69dac94205a3f2cec83b1c6d743d0de689478
Max Zlotskiy <max.zlotskiy@gmail.com>
2025-11-30 19:54:38 -0500

    added event spending to API

--- frontend/models/Event.js
+++ frontend/models/Event.js
@@ -1,5 +1,6 @@
 const pb = require("../api")
 const dayjs = require("../dayjs-lib")
+const util = require("../util/index")
 
 const byId = {};
 let list = [];
@@ -11,14 +12,40 @@ var Event = {
 	},
 
 	loadListHelper: function(events) {
-		for(event of events) {
-			byId[event["id"]] = event;
-		}
-		list = events;
+		list = util.groupBy(
+			events,
+			["event_id"],
+			function(event_id_list, recordsInGroup) {
+				let event_id = event_id_list[0];
+				return byId[event_id] = {
+					id: recordsInGroup[0]["event_id"],
+					name: recordsInGroup[0]["event_name"],
+					icon: recordsInGroup[0]["event_icon"],
+					note: recordsInGroup[0]["event_note"],
+					start_date: recordsInGroup[0]["event_start_date"],
+					end_date: recordsInGroup[0]["event_end_date"],
+					summary_by_wallet: recordsInGroup.map(r => ({
+						wallet: r.wallet,
+						currencyId: r.currency,
+						expenses: r.expenses,
+						income: r.income,
+						money: r.income - r.expenses,
+					})),
+					// summary_by_wallet: util.listToMap(recordsInGroup, "wallet", ["wallet", "currency", "expenses", "income"]),
+					summary_by_currency: util.groupBy(recordsInGroup, ["currency"], function(currency, walletSummaries) {
+						return {
+							currencyId: currency[0],
+							expenses: walletSummaries.reduce((sum, current) => sum + current.expenses, 0),
+							income: walletSummaries.reduce((sum, current) => sum + current.income, 0),
+							money: walletSummaries.reduce((sum, current) => sum + current.income - current.expenses, 0)
+						};
+					})
+				}
+			})
 	},
 
 	loadList: function() {
-		return pb.collection("events").getFullList().then(Event.loadListHelper)
+		return pb.collection("events_progress").getFullList().then(Event.loadListHelper)
 	},
 
 	getCurrentDuring: function(date) {
--- frontend/pages/event_details.jsx
+++ frontend/pages/event_details.jsx
@@ -1,15 +1,55 @@
 // presentation layer imports
 const m = require("mithril")
-const {Breadcrumb, createBreadcrumb, Button} = require("mithril-materialized")
-const DataProvider = require("../components/DataProvider.jsx")
-const EventSummary = require("../components/EventSummary.jsx")
+const {Breadcrumb, Button, Collapsible, DataTable} = require("mithril-materialized")
 const CategoryPieChart = require("../components/CategoryPieChart.jsx")
+const DataProvider = require("../components/DataProvider.jsx")
+const MoneyAmount = require("../components/MoneyAmount.jsx")
+const MoneyAmounts = require("../components/MoneyAmounts.jsx")
 const TransactionList = require("../components/TransactionList.jsx")
+const WalletChip = require("../components/WalletChip.jsx")
 
 // data layer imports
 const Event = require("../models/Event")
 const Report = require("../models/Report")
 const Transaction = require("../models/Transaction")
+
+const dayjs = require("../dayjs-lib")
+
+
+
+const WalletChipWrapper = {
+	view: function(vnode) {
+		return m(WalletChip, {id: vnode.attrs.value});
+	}
+}
+
+const MoneyAmountWrapper = {
+	view: function(vnode) {
+		const {value, row, index, column} = vnode.attrs;
+		return m(MoneyAmount, {
+			direction: column.field === "income"? Transaction.DIRECTION_INCOME : 
+				(column.field === "expenses"? Transaction.DIRECTION_EXPENSE :
+					(row.expenses > row.income? Transaction.DIRECTION_EXPENSE : Transaction.DIRECTION_INCOME)),
+			money: Math.abs(value),
+			currencyId: row.currencyId
+		})
+	}
+}
+
+const WalletBalanceSheet = {
+	view: function(vnode) {
+		return m(DataTable, {
+			title: "Wallet cash flow",
+			data: vnode.attrs.data,
+			columns: [
+				{key: "wallet", title: "Wallet", field: "wallet", sortable: true, filterable: true, cellRenderer: WalletChipWrapper},
+				{key: "income", title: "Income", field: "income", sortable: true, filterable: true, cellRenderer: MoneyAmountWrapper},
+				{key: "expenses", title: "Expenses", field: "expenses", sortable: true, filterable: true, cellRenderer: MoneyAmountWrapper},
+				{key: "money", title: "Net", field: "money", sortable: true, filterable: true, cellRenderer: MoneyAmountWrapper},
+			]
+		})
+	}
+};
 
 
 /**
@@ -61,13 +101,21 @@ module.exports = function() {
 						})
 					]),
 				]),
-				m("ul.collection", 
-					m(EventSummary, {event: event})),
+				m("div.container", [
+					m("h2", event.name),
+					m("p", "From " + dayjs(event.start_date).formatDate() + " to " + dayjs(event.end_date).formatDate() + ". " + (event.note || "")),
+					m("div", [
+						m(WalletBalanceSheet, {data: event.summary_by_wallet}),
+						m("strong", "Total: "),
+						m(MoneyAmounts, {list: event.summary_by_currency})
+					])
+				]),
 				m(DataProvider, {
 					fetch: Transaction.getWithFilter,
 					filter: {event: event.id},
 					viewWithData: (transactions) => ([
 						m(CategoryPieChart, {data: Report.transactionsToMoneyPerCategory(transactions)}),
+						m("h3", "Transactions"),
 						m(TransactionList, {transactions: transactions})
 					])
 				})
