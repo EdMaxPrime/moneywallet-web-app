4a6aa4a33ac01a9e7fae2c486e4ca99ebcd7023d
Max Zlotskiy <max.zlotskiy@gmail.com>
2026-01-13 16:53:26 -0500

    use new Filter

--- frontend/pages/transaction_search.jsx
+++ frontend/pages/transaction_search.jsx
@@ -4,6 +4,7 @@ const TransactionListPageable = require("../components/TransactionListPageable")
 const TransactionSearch = require("../components/TransactionSearch")
 
 const Transaction = require("../models/Transaction")
+const TransactionBulkEditForm = require("../models/TransactionBulkEditForm")
 
 const dayjs = require("../dayjs-lib")
 const currency = require("currency.js")
@@ -21,53 +22,11 @@ const searchEventListener = function(search) {
  * to this component.
  */
 const convertSearchQueryToFetchArg = function(searchQuery) {
-	let optionalFilter = ["description ~ '" + searchQuery.searchTerm + "'"]; // these are joined by logical OR
-	let requiredFilter = []; // these are joined by logical AND
-	let expand = ["data_source"]; // related objects that need to be queried, such as the place
-
-	// construct optional filter
-	if(searchQuery.matchNote) {
-		optionalFilter.push("note ~ '" + searchQuery.searchTerm + "'");
-	}
-	if(searchQuery.matchCategory) {
-		expand.push("category");
-		optionalFilter.push("category.name ~ '" + searchQuery.searchTerm + "'");
-	}
-	if(searchQuery.matchEvent) {
-		expand.push("event");
-		optionalFilter.push("event.name ~ '" + searchQuery.searchTerm + "'");
-	}
-	if(searchQuery.matchPlace) {
-		expand.push("place");
-		optionalFilter.push("place.name ~ '" + searchQuery.searchTerm + "'");
-	}
-	if(searchQuery.matchPeople) {
-		expand.push("people");
-		optionalFilter.push("people.name ?~ '" + searchQuery.searchTerm + "'");
-	}
-
-	// create order of operations
-	requiredFilter.push("(" + optionalFilter.join(" || ") + ")");
-
-	// construct required filter
-	if(searchQuery.after && dayjs(searchQuery.after, "YYYY-MM-DD", true).isValid()) {
-		requiredFilter.push("date >= '" + searchQuery.after + "'");
-	}
-	if(searchQuery.before && dayjs(searchQuery.before, "YYYY-MM-DD", true).isValid()) {
-		requiredFilter.push("date <= '" + searchQuery.before + "'");
-	}
-	if(searchQuery.min) {
-		requiredFilter.push("money >= " + currency(searchQuery.min).intValue);
-	}
-	if(searchQuery.max) {
-		requiredFilter.push("money <= " + currency(searchQuery.max).intValue);
-	}
-
 	// TODO: the size of the response can be reduced by specifying {fields: "*"} to exclude child entities
 	return {
 		sort: "-date",
-		filter: requiredFilter.join(" && "),
-		expand: expand.join(","),
+		filter: new Transaction.Filter(searchQuery).toString(true),
+		expand: "data_source,people,place,event,category",
 	};
 }
 
@@ -82,6 +41,7 @@ module.exports = function() {
 
 			return (<div>
  				<TransactionSearch values={search} onSearch={searchEventListener} />
+ 				<m.route.Link href="/transactions/bulk_edit" onclick={() => TransactionBulkEditForm.current.startSessionFromSearch(new Transaction.Filter(search)).finally(m.redraw)}>Bulk Edit</m.route.Link>
  				<TransactionListPageable 
 					arg={convertSearchQueryToFetchArg(search)} 
 					fetch={Transaction.loadSome}
